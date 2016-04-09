ea3be62eefe3c8901ae9d4c104b75d9f32a99018
HE <931280409@qq.com>
2016-04-09 09:02:30 +0800

    12

--- HWMusic/HMainViewController.h
+++ HWMusic/HMainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HMainViewController.h
 //  HWMusic
 //
 //  Created by admin on 16/4/9.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HMainViewController : UIViewController
 
 @end
-
